20b158eae9e0f7402e606199344aeec5d3cfa094
sjoshi <skjoshi13@gmail.com>
2018-12-20 11:48:31 +1100

    Initial Commits

---
+++ src/main/java/com/arq/lytmus/LytmusApplication.java
@@ -0,0 +1,14 @@
+package com.arq.lytmus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LytmusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LytmusApplication.class, args);
+	}
+
+}
+
